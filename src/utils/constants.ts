1721bce507aa561a547b35bb1330f0ac4dbc8ba4
shubham-gravitii <shubham@gravitii.xyz>
2024-09-24 02:41:57 +0530

    base url changed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL:string  = "https://notemaker-backend-two.vercel.app";
-export const BASE_URL:string  = "http://localhost:5000";
+export const BASE_URL:string  = "https://notemaker-backend-two.vercel.app";
+// export const BASE_URL:string  = "http://localhost:5000";
